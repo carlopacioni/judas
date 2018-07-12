a9cb83823bcf913f6d5208e8c4c73b6c9eaebed3
Carlo Pacioni <carlo.pacioni@gmail.com>
2018-07-12 17:29:35 +1000

    finalised Effort. Intercept point function missing

--- Effort.R
+++ Effort.R
@@ -16,11 +16,35 @@ judas.master[, REGION := sub(pattern = " +", replacement = "", x = REGION)]
 judas.master[, Year:=year(EVENT_DATE)]
 judas.master[, Month:=month(EVENT_DATE)]
 
+# Insert a col with location (shire) where the animals have been tracked, e.g. 'TrackedShire'
+judas.master[, TrackedShire := *** your intercept function here ***]
+
+# Search and replace all SHIRE instances for TrackedShire and things should work
+
 # Count instances
 runs <- judas.master[, .N, by=c("EVENT_DATE", "Year", "Month", "REGION", "SHIRE")]
-runs_month <- runs[, .N, by=c("Year", "Month", "REGION", "SHIRE")]
+ncollared <- judas.master[ACTION == "COLLARED", .(Ncollared=.N),
+                          by=c("EVENT_DATE", "Year", "Month", "REGION", "SHIRE")]
+# Meerge together
+runs <- merge(runs, ncollared, all.x=T, 
+              on=c("EVENT_DATE", "Year", "Month", "REGION", "SHIRE"))
 
-ggplot(runs_month, aes(factor(Month), N)) + geom_histogram(stat="identity") + 
-  facet_grid(Year~REGION)
+runs_month <- runs[, .(.N, Ncollared=sum(Ncollared, na.rm = T)), 
+                   by=c("Year", "Month", "REGION", "SHIRE")]
+
+runs_month <- runs_month[, Effort := N * 7.5 - Ncollared * 0.5]
+surveyed_months <- runs_month[, .N, by=c("REGION", "SHIRE", "Month")]
+
+# Number of year of the program per region
+runs_month[, .(Min=min(Year), Max=max(Year), nYear=max(Year) - min(Year)), 
+           by=REGION]
+
+ggplot(surveyed_months, aes(factor(Month), N)) + geom_histogram(stat="identity") + 
+  facet_grid(SHIRE~REGION) + geom_hline(yintercept = 19, col="red") + 
+  geom_hline(yintercept = 23, col="blue")
+
+ggplot(data = runs_month, aes(x=factor(Month), y=Year)) +
+  geom_raster(aes(fill = N), interpolate = F) +
+  scale_fill_gradient(low = "yellow", high = "red") + facet_grid(SHIRE~REGION)
 
 write.csv(runs_month, file.path(data.path, "Effort.csv"), row.names = F)  
