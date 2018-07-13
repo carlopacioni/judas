2d4496fdfcd8e042b73da71549ea54a443bccfee
Carlo Pacioni <carlo.pacioni@gmail.com>
2018-07-13 17:46:07 +1000

    I've been too rushed

--- TotalHarvest.R
+++ TotalHarvest.R
@@ -57,19 +57,19 @@ judas.master[, TrackedShire:=
                ifelse(TrackedShire == "PHED" | TrackedShire == "EPIL", "PB",
                       ifelse(TrackedShire == "BROOME" | TrackedShire == "DERB", "WK", 
                              ifelse(TrackedShire == "HALL", "HC", 
-                                    ifelse(TrackedShire == "WYND", "EW", "other"))))]
-judas.master[LAT > -17 & LONG > 128.5, TrackedShire:="WK"]
-judas.master[LAT < -17 & LONG > 128.5, TrackedShire:="HC"]
-judas.master[LAT < -19.98 & LONG < 120.25, TrackedShire:="PB"]
+                                    ifelse(TrackedShire == "WYND", "EK", "other"))))]
+judas.master[LAT > -17 & LONG > 128.5 & TrackedShire == "other", TrackedShire:="EK"]
+judas.master[LAT < -17 & LONG > 128.5& TrackedShire == "other", TrackedShire:="HC"]
+judas.master[LAT < -19.98 & LONG < 120.25 & TrackedShire == "other", TrackedShire:="PB"]
 
 opp.shoot[, TrackedShire:=
                ifelse(TrackedShire == "PHED" | TrackedShire == "EPIL", "PB",
                       ifelse(TrackedShire == "BROOME" | TrackedShire == "DERB", "WK", 
                              ifelse(TrackedShire == "HALL", "HC", 
-                                    ifelse(TrackedShire == "WYND", "EW", "other"))))]
-opp.shoot[Latitude > -17 & Longitude > 128.5, TrackedShire:="WK"]
-opp.shoot[Latitude < -17 & Longitude > 128.5, TrackedShire:="HC"]
-opp.shoot[Latitude < -19.98 & Longitude < 120.25, TrackedShire:="PB"]
+                                    ifelse(TrackedShire == "WYND", "EK", "other"))))]
+opp.shoot[Latitude > -17 & Longitude > 128.5 & TrackedShire == "other", TrackedShire:="EK"]
+opp.shoot[Latitude < -17 & Longitude > 128.5 & TrackedShire == "other", TrackedShire:="HC"]
+opp.shoot[Latitude < -19.98 & Longitude < 120.25 & TrackedShire == "other", TrackedShire:="PB"]
 
 # Rm useless spaces in REGION
 judas.master[, REGION := sub(pattern=" +", replacement="", x=REGION)]
